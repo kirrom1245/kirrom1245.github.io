d99eb5065bd1a77aa6400ff3351aa42b0ad128b7
kirrom1245 <56156432+kirrom1245@users.noreply.github.com>
2020-09-15 01:34:21 +0500

    Add files via upload

---
+++ build/precache-manifest.adfd0ca1ed411fa087cdc38338b091e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eba95e4fc4bd5d79b81c907efe12a02",
+    "url": "/vk-apps-image-exampe/index.html"
+  },
+  {
+    "revision": "02990f4052db38f5c960",
+    "url": "/vk-apps-image-exampe/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "02990f4052db38f5c960",
+    "url": "/vk-apps-image-exampe/static/js/2.1086d83e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-apps-image-exampe/static/js/2.1086d83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4384832c5dd7fe6373",
+    "url": "/vk-apps-image-exampe/static/js/main.7f4a7754.chunk.js"
+  },
+  {
+    "revision": "b923b8c109993ef0a516",
+    "url": "/vk-apps-image-exampe/static/js/runtime-main.90a10473.js"
+  }
+]);
